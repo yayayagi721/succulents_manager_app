5ccee246e87297793a159410e8e4ac536a70522f
yayayagi721 <snoopy1995.1.25@gmail.com>
2021-06-27 03:04:49 +0900

    みずかさをUIで見れるように変更

--- lib/plant_list_view.dart
+++ lib/plant_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+// ignore: use_key_in_widget_constructors
 class PlantListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -64,81 +66,93 @@ class PlantListView extends StatelessWidget {
   Widget _menuItem(
       String name, String botanicalName, double moisture, int soilAgeOfDay) {
     return Container(
+        clipBehavior: Clip.antiAlias,
         height: 120,
         decoration: BoxDecoration(
-          color: Color.fromRGBO(207, 230, 193, 1),
+          color: Color.fromRGBO(218, 180, 153, 1),
           borderRadius: BorderRadius.circular(10),
         ),
         margin: EdgeInsets.only(top: 3, left: 3, right: 3, bottom: 9),
-        child: _cardBody(name, botanicalName, moisture, soilAgeOfDay,
-            Image.asset('sample_images/testImage.jpeg')));
+        child: Stack(
+          children: [
+            WaterParameter(
+              waterAmountPer: 1,
+            ),
+            Positioned(
+                child: _cardBody(name, botanicalName, moisture, soilAgeOfDay,
+                    Image.asset('sample_images/testImage.jpeg'))),
+          ],
+        ));
   }
 
   Widget _cardBody(String name, String botanicalName, double moisture,
       int soilAgeOfDay, Image plantImage) {
     return Column(
       children: <Widget>[
-        Expanded(
-            flex: 3,
-            child: Padding(
-              padding: EdgeInsets.only(top: 6, left: 6, right: 6),
+        Expanded(flex: 3, child: _mainArea(plantImage)),
+        Expanded(flex: 1, child: _indicatorArea()),
+      ],
+    );
+  }
+
+  Widget _mainArea(Image plantImage) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          // color: Color.fromRGBO(207, 230, 193, 1),
+          color: Color.fromRGBO(207, 230, 193, 1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: plantImage,
+                )),
+            Expanded(
+              flex: 7,
               child: Container(
-                decoration: BoxDecoration(
-                  // color: Color.fromRGBO(207, 230, 193, 1),
-                  color: Color.fromRGBO(255, 212, 137, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                        flex: 2,
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          child: plantImage,
-                        )),
-                    Expanded(
-                      flex: 7,
-                      child: Container(
-                        width: double.infinity,
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 25, left: 10),
-                          child: Column(
-                            children: [
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text("hoge",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20)),
-                              ),
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  "hoge",
-                                ),
-                              ),
-                            ],
-                          ),
+                width: double.infinity,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 25, left: 10),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text("hoge",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "hoge",
                         ),
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            )),
-        Expanded(
-            flex: 1,
-            child: Center(
-              child: Container(
-                  width: double.infinity,
-                  child: Row(
-                    children: [
-                      _stateBar(Icons.opacity, "18%"),
-                      _stateBar(Icons.terrain, "57日")
-                    ],
-                  )),
-            )),
-      ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _indicatorArea() {
+    return Center(
+      child: Container(
+          width: double.infinity,
+          child: Row(
+            children: [
+              _stateBar(Icons.opacity, "18%"),
+              _stateBar(Icons.terrain, "57日")
+            ],
+          )),
     );
   }
 
@@ -175,4 +189,49 @@ class PlantListView extends StatelessWidget {
   //   }
   //   return rows;
   // }
+}
+
+class WaterParameter extends StatefulWidget {
+  final double? waterAmountPer;
+
+  WaterParameter({this.waterAmountPer});
+
+  @override
+  _WaterParameterState createState() => _WaterParameterState();
+}
+
+class _WaterParameterState extends State<WaterParameter> {
+  Size? size;
+  final GlobalKey _targetKey = GlobalKey();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addPostFrameCallback((_) => getSizeAndPosition());
+
+    super.initState();
+  }
+
+  getSizeAndPosition() {
+    if (_targetKey.currentContext != null) {
+      size = _targetKey.currentContext!.size;
+    } else {
+      size = Size(100, 100);
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: size!.height * widget.waterAmountPer!,
+        key: _targetKey,
+        decoration: BoxDecoration(
+          // borderRadius: BorderRadius.circular(10),
+          color: Color.fromRGBO(150, 216, 224, 1),
+        ),
+      ),
+    );
+  }
 }
